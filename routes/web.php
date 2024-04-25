436829097a413e22c187569baa8db037f8ec5eb6
rjzim01 <rjzim01@gmail.com>
2024-04-25 14:24:03 +0600

    6_login_page_changed

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CreateController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\auth\ProviderController;
+use App\Http\Controllers\Auth\ProviderController;
 
 Route::get('/', function () {
     // return view('welcome');
